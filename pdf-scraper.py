af29547db4dd576cabbc90617d1b6e70db9b8105
Aliya Gamez <aliya.e.gamez@gmail.com>
2026-04-01 15:07:57 -0400

    Add PDF title extraction and improve CSV output format

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -5,6 +5,7 @@ from io import BytesIO as bi
 
 import requests as rq
 import pandas as pd
+from rich import print
 from bs4 import BeautifulSoup as bs
 from bs4 import SoupStrainer as ss
 from pypdf import PdfReader as pr
@@ -37,12 +38,15 @@ def getPdfFilename(href):
     filename = unquote(filename)
     return filename
 
-def getPdfTitle(href):
+def getPdfTitle(href,name):
     if GET_PDF_TITLES == False: return 'Titles Scraping Disabled'
     r = rq.get(href)
     file = bi(r.content)
     reader = pr(file)
-    return reader.metadata.title;
+    title = reader.metadata.title
+    if(title): print('[green]Yes Title:[/green]\t' + '[link=' + href + ']'+ title +'[/link]')
+    else: print('[red]No Title:[/red]\t' + '[link=' + href + ']'+ name +'[/link]')
+    return title;
 
 def convertRelativeUrl(href):
     if href.startswith("/"): href = "https://" + DOMAIN + href
@@ -54,13 +58,16 @@ def main():
     content = loadPageLinks("https://career.fsu.edu/resources/career-guides")
     for c in content:
         # grab HREF from link list (c) and convert to actual url for 
-        href = convertRelativeUrl(c.get('href'));
+        href = convertRelativeUrl(c.get('href'))
         if(href) and pdfFilter(href): 
+            filename = getPdfFilename(href)
+            title = getPdfTitle(href,filename)
             columns = {
                 "url": href,
-                "file name": getPdfFilename(href),
-                "title": getPdfTitle(href)
+                "file_name": filename,
+                "title": title
             }
+            if(GET_PDF_TITLES==False): columns.pop("title", None)
             rows.append(columns)
 
     # Output to CSV
